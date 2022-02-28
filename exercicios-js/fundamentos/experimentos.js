194d89006e43b9210188933d24643e720dd4d523
João Luiz Ramos <joaoluizramos96@gmail.com>
2022-02-28 03:32:45 -0300

    Experimentos de JS

---
+++ exercicios-js/fundamentos/experimentos.js
@@ -0,0 +1,18 @@
+let a = 3;
+
+global.b = 123;
+
+this.c = 456;
+this.d = false;
+this.e = "teste";
+
+console.log(a);
+console.log(global.b);
+console.log(this.c);
+console.log(module.exports.c);
+console.log(module.exports === this);
+console.log(module.exports);
+
+// Criando variável maluca: sem let e var
+abc = 3; // NUNCA FAÇA ISSO
+console.log(global.abc);
